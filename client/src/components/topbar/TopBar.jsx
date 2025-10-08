a923df6d9e6c6a7d0ece9084ba8b6244c6ecc93d
Bhavna Rathore <bhavnagithub@gmail.com>
2025-10-08 10:13:55 +0530

    Make the Social media icon working

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -5,22 +5,31 @@ import "./topbar.css"
 
 export default function TopBar() {
   const { user, dispatch } = useContext(Context);
-const PF = "http://localhost:5000/images/"
+  const PF = "http://localhost:5000/images/"
 
   const handleLogout = () => {
     dispatch({ type: "LOGOUT" });
   };
 
-    return(
-        <div className ="top">
-           <div className="topLeft">
-           <i className=" topIcon fa-brands fa-square-facebook"></i>
-           <i className=" topIcon fa-brands fa-square-twitter"></i>
-           <i className=" topIcon fa-brands fa-square-instagram"></i>
-           <i className=" topIcon fa-brands fa-square-pinterest"></i> 
-           </div>
-           <div className="topCenter">
-           <ul className="topList">
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <a href="https://facebook.com" target="_blank" >
+          <i className="topIcon fa-brands fa-square-facebook"></i>
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noreferrer">
+          <i className="topIcon fa-brands fa-square-twitter"></i>
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noreferrer">
+          <i className="topIcon fa-brands fa-square-instagram"></i>
+        </a>
+        <a href="https://pinterest.com" target="_blank" rel="noreferrer">
+          <i className="topIcon fa-brands fa-square-pinterest"></i>
+        </a>
+      </div>
+
+      <div className="topCenter">
+        <ul className="topList">
           <li className="topListItem">
             <Link className="link" to="/">HOME</Link>
           </li>
@@ -31,32 +40,32 @@ const PF = "http://localhost:5000/images/"
           <li className="topListItem"><Link className="link" to="/write">WRITE</Link>
           </li>
           <li className="topListItem" onClick={handleLogout}>
-          {user && "LOGOUT" }
+            {user && "LOGOUT"}
           </li>
 
         </ul>
-           </div>
-           <div className="topRight">
-           {user ? (
+      </div>
+      <div className="topRight">
+        {user ? (
           <Link className="link" to="/settings">
             <img
-             className="topImg"src={PF+user.profilePic} alt="" />
-            </Link>
-             ) : (
-                <ul className="topList">
-                  <li className="topListItem">
-                    <Link className="link" to="/login">LOGIN </Link>
-                  </li>
-                  <li className="topListItem">
-                    <Link className="link" to="/register">REGISTER</Link>
-                  </li>
-                </ul>
-              )}
-            <i className="topSearchicon fa-solid fa-magnifying-glass"></i>
-           </div>
+              className="topImg" src={PF + user.profilePic} alt="" />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">LOGIN </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">REGISTER</Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchicon fa-solid fa-magnifying-glass"></i>
+      </div>
 
-        </div>
-    )
+    </div>
+  )
 }
 
 
